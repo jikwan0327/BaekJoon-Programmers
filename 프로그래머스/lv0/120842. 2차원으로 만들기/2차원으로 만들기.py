1b2ab0fb7635276719ab6af999d279a94fd56be5
jikwan0327 <102665117+jikwan0327@users.noreply.github.com>
2023-03-24 12:21:15 +0900

    [level 0] Title: 2차원으로 만들기, Time: 0.02 ms, Memory: 28.2 MB -BaekjoonHub

--- 프로그래머스/lv0/120842. 2차원으로 만들기/2차원으로 만들기.py
+++ 프로그래머스/lv0/120842. 2차원으로 만들기/2차원으로 만들기.py
@@ -1,6 +1,13 @@
+# def solution(num_list, n):
+#     answer = []
+#     for i in range(int(len(num_list)/n)):
+#         for j in range(0, n):
+#             answer[i][j] = num_list[i*n+j]
+#     return answer
+
+import numpy as np
 def solution(num_list, n):
-    answer = [[0] * n for _ in range(int(len(num_list)/n))]
-    for i in range(int(len(num_list)/n)):
-        for j in range(0, n):
-            answer[i][j] = num_list[i*n+j]
-    return answer
+    answer = []
+    a = np.array(num_list).reshape(int(len(num_list)/n), n)
+    answer = a
+    return answer.tolist()
