fdf3762f7c7ef7cbef00cfa6f7b62f4a3b3490bb
jikwan0327 <102665117+jikwan0327@users.noreply.github.com>
2023-07-22 10:45:40 +0900

    [level 0] Title: 한 번만 등장한 문자, Time: 0.08 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/lv0/120896. 한 번만 등장한 문자/한 번만 등장한 문자.js
+++ 프로그래머스/lv0/120896. 한 번만 등장한 문자/한 번만 등장한 문자.js
@@ -1,30 +1,22 @@
 function solution(s) {
     var answer = s.split('').sort();
+    let i =0;
     
     
-    for(let i = 0; i < s.length; i++) {
+    while(1) {
         if(answer[i] == answer[i+1]){
             answer = answer.filter((res) => res !== answer[i])
+            i = 0;
+        } else {
+            i++;
         }
-    }
-    
-    for(let i = 0; i < s.length; i++) {
-        if(answer[i] == answer[i+1]){
-            answer = answer.filter((res) => res !== answer[i])
+        
+        if(answer[-1] == answer[i]) {
+            break;
         }
+        
     }
-    
-    for(let i = 0; i < s.length; i++) {
-        if(answer[i] == answer[i+1]){
-            answer = answer.filter((res) => res !== answer[i])
-        }
-    }
-    
-    for(let i = 0; i < s.length; i++) {
-        if(answer[i] == answer[i+1]){
-            answer = answer.filter((res) => res !== answer[i])
-        }
-    }
+
 
     return answer.sort().join('');
 }
