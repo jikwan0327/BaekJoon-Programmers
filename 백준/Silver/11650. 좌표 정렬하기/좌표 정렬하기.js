a72ae927981cd0a891f29e5a2758cbb42240772d
jikwan0327 <102665117+jikwan0327@users.noreply.github.com>
2023-07-26 09:11:20 +0900

    [Silver V] Title: 좌표 정렬하기, Time: 524 ms, Memory: 60880 KB -BaekjoonHub

--- 백준/Silver/11650. 좌표 정렬하기/좌표 정렬하기.js
+++ 백준/Silver/11650. 좌표 정렬하기/좌표 정렬하기.js
@@ -1,18 +1,20 @@
 let fs = require('fs');
 let input = fs.readFileSync('/dev/stdin').toString().split('\n');
-let n = Number(input[0]);
-let data = [];
-for (let i = 1; i <= n; i++) {
-let [x, y] = input[i].split(' ').map(Number);
-data.push([x, y]);
+let arr = []
+
+for(let i = 1; i <= Number(input[0]); i++) {
+    [x, y] = input[i].split(' ').map((res) => Number(res))
+    arr.push([x, y])
 }
-function compare (a, b) {
-if (a[0] != b[0]) return a[0] - b[0]; // x 좌표 기준 오름차순
-else return a[1] - b[1]; // x가 같으면 y 좌표 기준 오름차순
-}
-data.sort(compare); // 정렬 수행
-let answer = ""; // 정렬 결과 출력
-for (let point of data) {
+arr.sort((a,b) => {
+    if(a[0] == b[0]) {
+        return a[1] - b[1]
+    }else{
+        return a[0] - b[0];
+    }
+})
+let answer = ""; 
+for (let point of arr) {
 answer += point[0] + " " + point[1] + "\n";
 }
 console.log(answer);
