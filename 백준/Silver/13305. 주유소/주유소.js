36518089778149592ed27e4d57296102cb38e8ba
jikwan0327 <102665117+jikwan0327@users.noreply.github.com>
2023-08-05 13:40:49 +0900

    [Silver III] Title: 주유소, Time: 216 ms, Memory: 20584 KB -BaekjoonHub

--- 백준/Silver/13305. 주유소/주유소.js
+++ 백준/Silver/13305. 주유소/주유소.js
@@ -1,14 +1,18 @@
 let fs = require("fs");
 let input = fs.readFileSync("/dev/stdin").toString().split('\n');
+
+let n = Number(input[0])
 let line2 = input[1].split(' ').map(Number)
 let line3 = input[2].split(' ').map(Number)
-let min = line3[0]
-let total = 0;
 
-for(let i = 0; i < Number(input[0]) - 1; i++) {
-    if(line3[i] < min) {
-        min = line3[i]
-    }
+let min = line3[0]
+for(let i = 0; i < n; i++) {
+    min = Math.min(min, line3[i])
+    line3[i] = min; 
+}
+
+let total = 0;
+for(let i = 0; i < n - 1; i++) {
     total += min * line2[i]
 }
 console.log(total)
