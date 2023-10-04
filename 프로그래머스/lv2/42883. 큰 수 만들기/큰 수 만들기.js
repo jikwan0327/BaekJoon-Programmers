b28af93b9dcf8f930745ecebeb9531737dc6b476
jikwan0327 <102665117+jikwan0327@users.noreply.github.com>
2023-10-04 18:58:22 +0900

    [level 2] Title: 큰 수 만들기, Time: 0.12 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/lv2/42883. 큰 수 만들기/큰 수 만들기.js
@@ -0,0 +1,26 @@
+function solution(number, k) {
+    var answer = []
+    let stack = []
+    let arr = number.split('').map(Number)
+    let i = 1;
+    
+    answer.push(arr[0])
+    for(let i = 1; i < number.length; i++) {
+        if(stack.length != k) {
+            if(answer[answer.length-1] < arr[i]) {
+                while(answer[answer.length-1] < arr[i] && stack.length != k)  {
+                    answer.pop()
+                    stack.push(arr[i])
+                }
+                answer.push(arr[i])
+            }
+            else {
+                answer.push(arr[i])
+            }
+        } else {
+            answer.push(arr[i])
+        }
+    } 
+    
+    return answer.filter((_, idx) => idx < number.length - k).join('');
+}
