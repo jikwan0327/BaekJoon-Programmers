61e90d5ad1e6d1daed63c7bcda4539007c6a48aa
jikwan0327 <102665117+jikwan0327@users.noreply.github.com>
2023-10-10 19:38:20 +0900

    [level 2] Title: 주식가격, Time: 4.62 ms, Memory: 42.3 MB -BaekjoonHub

---
+++ 프로그래머스/2/42584. 주식가격/주식가격.js
@@ -0,0 +1,15 @@
+function solution(prices) {
+    var answer = [];    
+    for(let i = 0; i < prices.length; i++) {
+        let current = prices[i];
+        let result = 0;
+        for(let j = i + 1; j < prices.length; j++) {
+            result += 1
+            if(current > prices[j]) {
+                break;
+            }
+        }
+        answer.push(result)
+    }
+    return answer;
+}
