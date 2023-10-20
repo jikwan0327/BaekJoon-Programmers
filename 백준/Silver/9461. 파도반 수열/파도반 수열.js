6448732a69df18a91e2e8181e6ae87b5677dda89
jikwan0327 <102665117+jikwan0327@users.noreply.github.com>
2023-10-20 15:49:20 +0900

    [Silver III] Title: 파도반 수열, Time: 136 ms, Memory: 9844 KB -BaekjoonHub

---
+++ 백준/Silver/9461. 파도반 수열/파도반 수열.js
@@ -0,0 +1,18 @@
+let fs = require('fs')
+let input = fs.readFileSync('/dev/stdin').toString().split('\n').map(Number);
+input.pop()
+let first = input.shift()
+let d = new Array(Math.max(...input) + 1).fill(0).map((_, idx) => idx);
+d[1] = 1;
+d[2] = 1;
+d[3] = 1;
+d[4] = 2;
+d[5] = 2;
+
+for(let j = 6; j < Math.max(...input) + 1; j++) {
+    d[j] = d[j - 1] + d[j-5]
+}
+
+for(let i = 0; i < first; i++) {
+    console.log(d[input[i]])
+}
