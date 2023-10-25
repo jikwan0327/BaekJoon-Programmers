d6931001a076621f1040f4c387e909271d7d2cc6
jikwan0327 <102665117+jikwan0327@users.noreply.github.com>
2023-10-25 20:19:20 +0900

    [level 2] Title: 삼각 달팽이, Time: 30.39 ms, Memory: 101 MB -BaekjoonHub

---
+++ 프로그래머스/2/68645. 삼각 달팽이/삼각 달팽이.js
@@ -0,0 +1,58 @@
+function solution(n) {
+    var answer = [];
+    let s = 0;
+    let x = 0;
+    let y = 0;
+    let direction = "bottom"
+    for(let i = 1; i <= n; i++) {
+        s += i;
+    }
+    const arr = new Array(n)
+    for (let i = 0; i < n; i++) {
+        arr[i] = new Array(i+1).fill(0)
+    }
+    
+    arr[0][0] = 1
+    for(let i = 2; i <= s; i++) {
+        if(direction == 'bottom') {
+            y++
+            if(y == n || arr[y][x] != 0) {
+                y--;
+                x++;
+                direction = 'right'
+                arr[y][x] = i
+            } else {
+                arr[y][x] = i
+            }
+        } else if(direction == 'right') {
+            x++
+            if(x == n || arr[y][x] != 0) {
+                y--;
+                x-=2;
+                direction = 'top'
+                arr[y][x] = i
+            } else {
+                arr[y][x] = i
+            }
+        } else if(direction == 'top') {
+            y--;
+            x--;
+            if(arr[y][x] != 0) {
+                x++;
+                y+=2;
+                direction = 'bottom'
+                arr[y][x] = i
+            } else {
+                arr[y][x] = i
+            }
+        }
+    }
+    
+    for(let i = 0; i < n; i++) {
+        for(let j = 0; j < i+1; j++) {
+            answer.push(arr[i][j])
+        }
+    } 
+
+    return answer;
+}
