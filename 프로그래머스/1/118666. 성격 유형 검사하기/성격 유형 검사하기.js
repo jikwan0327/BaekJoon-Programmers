5df026b6f1ccfd648c0ec516a49318602e604379
jikwan0327 <102665117+jikwan0327@users.noreply.github.com>
2023-11-01 20:07:03 +0900

    [level 1] Title: 성격 유형 검사하기, Time: 3.71 ms, Memory: 33.7 MB -BaekjoonHub

---
+++ 프로그래머스/1/118666. 성격 유형 검사하기/성격 유형 검사하기.js
@@ -0,0 +1,39 @@
+function solution(survey, choices) {
+    var answer = [];
+    const object = new Map([
+        ["RT", 0], ["CF", 0], ["JM", 0], ["AN", 0], 
+        ["TR", 0], ["FC", 0], ["MJ", 0], ["NA", 0]
+    ])
+    
+    survey.map((res, idx) => {
+        object.set(res, object.get(res) + choices[idx] - 4)
+    })
+    
+    console.log(object)
+    
+    if(object.get('RT') - object.get('TR') <= 0) {
+        answer.push('R')
+    } else {
+        answer.push('T')
+    }
+    
+    if(object.get('CF') - object.get('FC') <= 0) {
+        answer.push('C')
+    } else {
+        answer.push('F')
+    }
+    
+    if(object.get('JM') - object.get('MJ') <= 0) {
+        answer.push('J')
+    } else {
+        answer.push('M')
+    }
+    
+    if(object.get('AN') - object.get('NA') <= 0) {
+        answer.push('A')
+    } else {
+        answer.push('N')
+    }
+    
+    return answer.join('');
+}
