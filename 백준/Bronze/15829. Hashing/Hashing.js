c98a49e447ada74de9e79eeb7564b2e9704bdad6
jikwan0327 <102665117+jikwan0327@users.noreply.github.com>
2024-03-19 12:17:53 +0900

    [Bronze II] Title: Hashing, Time: 128 ms, Memory: 9584 KB, Score: 100 point -BaekjoonHub

--- 백준/Bronze/15829. Hashing/Hashing.js
+++ 백준/Bronze/15829. Hashing/Hashing.js
@@ -2,10 +2,15 @@ const fs = require('fs')
 const input = fs.readFileSync('/dev/stdin').toString().trim().split('\n')
 const n = +input[0]
 const arr = input[1].split('')
-let total = 0
+let r = 1;
+const M = 1234567891;
+let hash = 0;
 
 arr.map((res, idx) => {
-    total += (res.charCodeAt() - 96) * 31**idx 
+    hash += (res.charCodeAt() - 96) * r
+    r *= 31
+    r %= M
+    hash %= M
 })
 
-console.log(total)
+console.log(hash)
