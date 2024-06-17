133f386bc0c1b7e8690b8c9e69049b66f4e417db
jikwan0327 <102665117+jikwan0327@users.noreply.github.com>
2024-06-17 13:56:32 +0900

    [Silver V] Title: 창문 닫기, Time: 160 ms, Memory: 10108 KB -BaekjoonHub

--- 백준/Silver/13909. 창문 닫기/창문 닫기.js
+++ 백준/Silver/13909. 창문 닫기/창문 닫기.js
@@ -2,7 +2,7 @@ const fs = require('fs')
 const input = +fs.readFileSync('/dev/stdin').toString()
 let result = 0
 
-for(let i = 1; i <= input; i++) {
+for(let i = 1; i <= input/2 + 1; i++) {
     if(i**2 <= input) result += 1;
     else break;
 }
